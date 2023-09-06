28fd069c681e02d4c55e76698004a260ab69dc1f
kkkedik <vladkeda8@gmail.com>
2023-09-06 07:09:32 +0300

    correction computer class

--- src/main/java/com/study/kedik/player/Computer.java
+++ src/main/java/com/study/kedik/player/Computer.java
@@ -10,7 +10,7 @@ public class Computer implements Player {
         if (board.isCellEmpty(1, 1)) {
             return new ImmutablePair<>(1, 1);
         } else {
-            return getWinStep(board, 0, 1, 2);
+            return getWinStep(board);
         }
     }
 
@@ -18,176 +18,93 @@ public class Computer implements Player {
         return board.isCellFilledWithChar(x1, y1, symbol) && board.isCellFilledWithChar(x2, y2, symbol) && board.isCellEmpty(x3, y3);
     }
 
-    public Pair<Integer, Integer> checkLineStep(Board board, int line, char symbol) {
-        if (line == 1) {
-            //строка 1
-            if (checkStep(board, 0, 0, 0, 2, 0, 1, symbol)) {
-                return new ImmutablePair<>(0, 1);
-            }
-        } else if (line == 2) {
-            //строка 2
-            if (checkStep(board, 1, 0, 1, 1, 1, 2, symbol)) {
-                return new ImmutablePair<>(1, 2);
-            }
-        } else {
-            //строка 3
-            if (checkStep(board, 2, 1, 2, 2, 2, 0, symbol)) {
-                return new ImmutablePair<>(2, 0);
-            }
-        }
-        return null;
-    }
-
     public Pair<Integer, Integer> checkColumnStep(Board board, int column, char symbol) {
-        if (column == 1) {
-            // столбец 1
-            if (checkStep(board, 0, 0, 2, 0, 1, 0, symbol)) {
-                return new ImmutablePair<>(1, 0);
-            }
-        } else if (column == 2) {
-            // столбец 2
-            if (checkStep(board, 0, 1, 1, 1, 2, 1, symbol)) {
-                return new ImmutablePair<>(2, 1);
-            }
-        } else {
-            // столбец 3
-            if (checkStep(board, 1, 2, 2, 2, 0, 2, symbol)) {
-                return new ImmutablePair<>(0, 2);
+
+        for (int i = 0; i < 3; i++) {
+            if (checkStep(board, column, (i + 2) % 3, column, (i + 1) % 3, column, i, symbol)) {
+                return new ImmutablePair<>(column, i);
             }
         }
         return null;
     }
 
-    public Pair<Integer, Integer> checkDiagonalStep(Board board, int diagonal, char symbol) {
-        switch (diagonal) {
-            case 1: {
-                if (checkStep(board, 0, 0, 2, 2, 1, 1, symbol)) {
-                    return new ImmutablePair<>(1, 1);
-                }
-                break;
+    public Pair<Integer, Integer> checkLineStep(Board board, int line, char symbol) {
+
+        for (int i = 0; i < 3; i++) {
+            if (checkStep(board, (i + 2) % 3, line, (i + 1) % 3, line, i, line, symbol)) {
+                return new ImmutablePair<>(i, line);
             }
-            case 2: {
-                if (checkStep(board, 2, 0, 0, 2, 1, 1, symbol)) {
-                    return new ImmutablePair<>(1, 1);
-                }
+        }
+        return null;
+    }
+
+    public Pair<Integer, Integer> checkDiagonalStep(Board board, char symbol) {
+
+        for (int i = 0; i < 3; i++) {
+            if (checkStep(board, (i + 2) % 3, (i + 2) % 3, (i + 1) % 3, (i + 1) % 3, i, i, symbol)) {
+                return new ImmutablePair<>(i, i);
             }
-            case 3: {
-                if (checkStep(board, 1, 1, 2, 2, 0, 0, symbol)) {
-                    return new ImmutablePair<>(0, 0);
-                }
-            }
-            case 4: {
-                if (checkStep(board, 1, 1, 0, 0, 2, 2, symbol)) {
-                    return new ImmutablePair<>(2, 2);
+            if (i == 0) {
+                if (checkStep(board, (i + 2) % 3, i, 1, 1, i, (i + 2) % 3, symbol)) {
+                    return new ImmutablePair<>(i, (i + 2) % 3);
                 }
             }
-            case 5: {
-                if (checkStep(board, 2, 0, 1, 1, 0, 2, symbol)) {
-                    return new ImmutablePair<>(0, 2);
-                }
-            }
-            case 6: {
-                if (checkStep(board, 0, 2, 1, 1, 2, 0, symbol)) {
-                    return new ImmutablePair<>(0, 1);
+            if (i == 2) {
+                if (checkStep(board, i, (i + 2) % 3, 1, 1, (i + 2) % 3, i, symbol)) {
+                    return new ImmutablePair<>(i, (i + 2) % 3);
                 }
             }
         }
         return null;
     }
 
-
-    public Pair<Integer, Integer> getWinStep(Board board, int zero, int one, int two) {
-
-        if (checkColumnStep(board, 0, 'O') != null) {
-            return checkColumnStep(board, 0, 'O');
-        }
-        if (checkColumnStep(board, 1, 'O') != null) {
-            return checkColumnStep(board, 1, 'O');
-        }
-        if (checkColumnStep(board, 2, 'O') != null) {
-            return checkColumnStep(board, 2, 'O');
+    public Pair<Integer, Integer> getWinStep(Board board) {
+        Pair<Integer, Integer> pair;
+        for (int i = 0; i < 3; i++) {
+            pair = checkColumnStep(board, i, 'O');
+            if (pair != null) {
+                return pair;
+            }
         }
 
-
-        if (checkLineStep(board, 0, 'O') != null) {
-            return checkLineStep(board, 0, 'O');
-        }
-        if (checkLineStep(board, 1, 'O') != null) {
-            return checkLineStep(board, 1, 'O');
-        }
-        if (checkLineStep(board, 2, 'O') != null) {
-            return checkLineStep(board, 2, 'O');
+        for (int i = 0; i < 3; i++) {
+            pair = checkLineStep(board, i, 'O');
+            if (pair != null) {
+                return pair;
+            }
         }
 
-        //Диагонали
-        if (checkDiagonalStep(board, 1, 'O') != null) {
-            return checkDiagonalStep(board, 1, 'O');
-        }
-        if (checkDiagonalStep(board, 2, 'O') != null) {
-            return checkDiagonalStep(board, 2, 'O');
-        }
-        if (checkDiagonalStep(board, 3, 'O') != null) {
-            return checkDiagonalStep(board, 3, 'O');
-        }
-        if (checkDiagonalStep(board, 4, 'O') != null) {
-            return checkDiagonalStep(board, 4, 'O');
-        }
-        if (checkDiagonalStep(board, 5, 'O') != null) {
-            return checkDiagonalStep(board, 5, 'O');
-        }
-        if (checkDiagonalStep(board, 6, 'O') != null) {
-            return checkDiagonalStep(board, 6, 'O');
+        pair = checkDiagonalStep(board, 'O');
+        if (pair != null) {
+            return pair;
         }
 
         //Х
-        if (checkColumnStep(board, 1, 'X') != null) {
-            return checkColumnStep(board, 1, 'X');
+
+        for (int i = 0; i < 3; i++) {
+            pair = checkColumnStep(board, i, 'X');
+            if (pair != null) {
+                return pair;
+            }
         }
-        if (checkColumnStep(board, 2, 'X') != null) {
-            return checkColumnStep(board, 2, 'X');
-        }
-        if (checkColumnStep(board, 3, 'X') != null) {
-            return checkColumnStep(board, 3, 'X');
+        for (int i = 0; i < 3; i++) {
+            pair = checkLineStep(board, i, 'X');
+            if (pair != null) {
+                return pair;
+            }
         }
 
-
-        if (checkLineStep(board, 1, 'X') != null) {
-            return checkLineStep(board, 1, 'X');
-        }
-        if (checkLineStep(board, 2, 'X') != null) {
-            return checkLineStep(board, 2, 'X');
-        }
-        if (checkLineStep(board, 3, 'X') != null) {
-            return checkLineStep(board, 3, 'X');
-        }
-
-        //Диагонали
-
-        if (checkDiagonalStep(board, 1, 'X') != null) {
-            return checkDiagonalStep(board, 1, 'X');
-        }
-        if (checkDiagonalStep(board, 2, 'X') != null) {
-            return checkDiagonalStep(board, 2, 'X');
-        }
-        if (checkDiagonalStep(board, 3, 'X') != null) {
-            return checkDiagonalStep(board, 3, 'X');
-        }
-        if (checkDiagonalStep(board, 4, 'X') != null) {
-            return checkDiagonalStep(board, 4, 'X');
-        }
-        if (checkDiagonalStep(board, 5, 'X') != null) {
-            return checkDiagonalStep(board, 5, 'X');
-        }
-        if (checkDiagonalStep(board, 6, 'X') != null) {
-            return checkDiagonalStep(board, 6, 'X');
+        pair = checkDiagonalStep(board, 'X');
+        if (pair != null) {
+            return pair;
         }
 
 
         if (board.isCellEmpty(0, 1)) {
             return new ImmutablePair<>(0, 1);
         }
-        if (board.isCellEmpty(0, 2)) {
-            return new ImmutablePair<>(0, 2);
+        if (board.isCellEmpty(2, 0)) {
+            return new ImmutablePair<>(2, 0);
         }
         if (board.isCellEmpty(2, 2)) {
             return new ImmutablePair<>(2, 2);
