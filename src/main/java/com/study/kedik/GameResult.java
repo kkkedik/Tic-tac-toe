dee656204aec548ffc78c12b68ebd59411c2503c
kkkedik <vladkeda8@gmail.com>
2023-09-30 17:20:27 +0300

    correction

---
+++ src/main/java/com/study/kedik/GameResult.java
@@ -0,0 +1,10 @@
+package com.study.kedik;
+
+import java.time.LocalDateTime;
+
+public class GameResult {
+    public String playerX;
+    public String playerO;
+    public GameStatus gameStatus;
+    LocalDateTime dateAndTimeOfGame;
+}
